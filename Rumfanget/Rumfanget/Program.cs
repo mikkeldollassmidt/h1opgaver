cfffe4fbccf4e7f24d9be3ebc78a9c8346eb5052
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2023-08-10 22:16:59 +0200

    Tilføjet opgave rumfang

---
+++ Rumfanget/Rumfanget/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumfanget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The console is writing the words inside of the ellipsis
+            Console.WriteLine("Enter length:");
+            // Making a double called length to capture whats getting typed and put inside of the variable.
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter width:");
+            // Making a double called width to capture whats getting typed and put inside of the variable.
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter height:");
+            // Making a double called height to capture whats getting typed and put inside of the variable.
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            // A double to calculate the volume.
+            double volume = height * width * length;
+
+            //Clearing the console for a cleaner view
+            Console.Clear();
+
+            // Writing the result.
+            Console.WriteLine("Volume = " + volume);
+
+            // Reads keyboard interaction to see when to stop.
+            Console.ReadLine();
+
+        }
+    }
+}
