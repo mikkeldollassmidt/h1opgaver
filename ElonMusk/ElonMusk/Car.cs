0577eaa0e6235e3d0b1fc4ecd8a0bbb212212381
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2023-09-11 22:05:37 +0200

    Tilføjet ElonMusk opgaven.

---
+++ ElonMusk/ElonMusk/Car.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Security;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ElonMusk
+{
+    public class Car
+    {
+        // Creating attributes
+        private byte _batteryCapacity = 100;
+        private string _color = "(Set color)";
+        private int _metresDriven = 0;
+
+        // Creating properties
+        public byte BatteryCapacity
+        {
+            get { return _batteryCapacity; }
+            private set { _batteryCapacity = value; }
+        }
+        public string Color
+        {
+            get { return _color; }
+            private set { _color = value; }
+        }
+        public int MetresDriven
+        {
+            get { return _metresDriven; }
+            private set { _metresDriven = value; }
+        }
+        // Method for choosing color.
+        public void ColorChoose()
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    _color = "Red";
+                    break;
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    _color = "Yellow";
+                    break;
+                case ConsoleKey.D3:
+                    Console.Clear();
+                    _color = "Green";
+                    break;
+                default:
+                    Console.Clear();
+                    _color = "Red";
+                    break;
+            }
+        }
+        // Method for displaying information about car 1 and car 2.
+        public void Display()
+        {
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine("Battery: " + BatteryCapacity + "%");
+            Console.WriteLine("Metres driven: " + MetresDriven + "\r\n");
+        }
+        // Runs cars
+        public void Run()
+        {
+
+            if (_batteryCapacity>=1)
+            {
+                _metresDriven += 20;
+                _batteryCapacity -= 1;
+            }
+            else
+            {
+                NeedChargeMessage();
+            }
+        }
+        // Charge car method
+        public void ChargeCar()
+        {
+            if (_batteryCapacity<=100)
+            {
+                _batteryCapacity = 100;
+            }
+            else
+            {
+                Console.WriteLine("Can't charge.");
+            }
+        }
+        // Writing a needs charging message.
+        public void NeedChargeMessage()
+        {
+            Console.WriteLine("Battery is too low.");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ ElonMusk/ElonMusk/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ElonMusk
+{
+    internal class Program
+    {
+        public Car car1 = new Car();
+
+        static void Main(string[] args)
+        {
+            // Creating an object
+            Car car1 = new Car();
+            Car car2 = new Car();
+
+            //Creating choose color GUI for car 1.
+            Console.Clear();
+            Console.WriteLine("Choose a color for car: \r\n1. Red\r\n2. Yellow\r\n3. Green");
+            car1.ColorChoose();
+            Console.WriteLine("Car is painted: " + car1.Color);
+            Thread.Sleep(1000);
+
+            //Creating choose color GUI for car 2.
+            Console.Clear();
+            Console.WriteLine("Choose a color for car: \r\n1. Red\r\n2. Yellow\r\n3. Green");
+            car2.ColorChoose();
+            Console.WriteLine("Car is painted: " + car2.Color);
+            Thread.Sleep(1000);
+
+            // Controls run and charge.
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Car 1:");
+                car1.Display();
+                Console.WriteLine("Car 2:");
+                car2.Display();
+                Console.WriteLine("Keys:\r\nRun: Car 1 = W, Car 2 = Y\r\nCharge: Car 1 = E, Car 2 = U");
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.W:
+                        car1.Run();
+                        break;
+                    case ConsoleKey.Y:
+                        car2.Run();
+                        break;
+                    case ConsoleKey.E:
+                        car1.ChargeCar();
+                        break;
+                    case ConsoleKey.U:
+                        car2.ChargeCar();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
