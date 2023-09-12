5e43b041e645cec1ec873e146206b02e0d6a4bf0
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2023-09-12 22:33:10 +0200

    Tilføjet BlackJack

---
+++ BlackJackStructExample/BlackJackStructExample/Model/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackStructExample.Model
+{
+    public struct Card
+    {
+        public int _cardDealerDeck { get; set; }
+        /*{
+            get { return _cardDealerDeck; }
+            set { _cardDealerDeck = value; }
+        }*/
+        public int _userDeck { get; set; }
+        /*{
+            get { return _userDeck; }
+            private set { _userDeck = value; }
+        }*/
+        public Card(int cardDealerDeck, int userDeck)
+        {
+            _cardDealerDeck = cardDealerDeck;
+            _userDeck = userDeck;
+        }
+        public override string ToString()  
+        {
+            return $"Dealer cards: {_cardDealerDeck}, User cards: {_userDeck}";
+        }
+    }
+}
---
+++ BlackJackStructExample/BlackJackStructExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackStructExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Model.Card decks = new Model.Card(3, 16);
+
+            string writeDecks = decks.ToString();
+
+            Console.WriteLine(writeDecks);
+            Console.ReadLine();
+        }
+    }
+}
