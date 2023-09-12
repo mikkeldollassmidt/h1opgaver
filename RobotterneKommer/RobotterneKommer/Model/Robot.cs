4698fde954e98bb1beb3eac4b897001a78c1bfe3
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2023-09-12 12:47:41 +0200

    Tilføjet Robotterne kommer

---
+++ RobotterneKommer/RobotterneKommer/Model/Robot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotterneKommer.Model
+{
+    internal class Robot
+    {
+        // Customization options. Attributes.
+        private bool _canSweep = true; // Default true. -- Every robot can sweep.
+        private bool _isBig = false; // Default false. -- Has wheels. Can sweep. It also has to polish windows OR change tires.
+        private bool _polishOrChangeTires = false; // Default false. If true, has RX microchip. And Polishing Windows. If false, has QT microchip. And changes tires.
+        private bool _hasWIFI = false; // Default false. -- Every robot can have wifi
+        private string _color = "white"; // Default white. -- Can only be changed if not polishwindows, and batteries.
+
+        // Changes value when attributes above changes values.
+        private byte _batteryCapacity = 255; // Default capacity 255.
+        private decimal _waterTankCapacity = 2.4M; // Default water tank capacity
+        private bool _hasWheels = false; // Default false. -- Has to be big, has to polish og change tires.
+        private bool _hasBatteries = true; // Default true. -- Cant if polish windows is true.
+
+        // This constructor is for fully customization.
+        public Robot(bool canSweep, bool isBig, bool polishOrChangeTires, bool hasWIFI, string color)
+        {
+            _canSweep = canSweep;
+            _isBig = isBig;
+            _polishOrChangeTires = polishOrChangeTires;
+            _hasWIFI = hasWIFI;
+            _color = color;
+        }
+        // This constructor is for customization without a color.
+        public Robot(bool canSweep, bool isBig, bool polishOrChangeTires, bool hasWIFI)
+        {
+            _canSweep = canSweep;
+            _isBig = isBig;
+            _polishOrChangeTires = polishOrChangeTires;
+            _hasWIFI = hasWIFI;
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            private set { _color = value; }
+        }
+    }
+}
---
+++ RobotterneKommer/RobotterneKommer/View/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotterneKommer
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Model.Robot r = new Model.Robot(true, true, true ,true, "green");
+            Console.WriteLine(r.Color);
+            Console.ReadLine();
+        }
+    }
+}
