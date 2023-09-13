32e2ac0bc7a66b808e50449361ead5d1fe68894d
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2023-09-13 09:10:55 +0200

    Tilføjet Fordelingshatten

---
+++ Fordelingshatten/Fordelingshatten/Controller/Controller.cs
@@ -0,0 +1,15 @@
+﻿using Fordelingshatten.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordelingshatten.Controller
+{
+    public class Controller
+    {
+        SortingHat s = new SortingHat();
+        
+    }
+}
---
+++ Fordelingshatten/Fordelingshatten/Hat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordelingshatten
+{
+    public class SortingHat
+    {
+        enum Colleges { Gryffindor, Hufflepuff, Ravenclaw, Slyhterin };
+        public int randomNumber;
+
+        public void ChooseCollege()
+        {
+            Random r = new Random();
+            randomNumber = r.Next(0, 4);
+
+            Console.WriteLine((Colleges)randomNumber);
+            Console.ReadLine();
+        }
+    }
+}
